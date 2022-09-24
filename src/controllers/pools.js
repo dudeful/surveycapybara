a6f9ee8f1ff9c6cf51a841b09c427e7991f4916e
danubiomuller <dudeful@outlook.com>
2022-09-24 11:46:43 -0300

    add endpoint for getting public pools

--- src/controllers/pools.js
+++ src/controllers/pools.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const newPool = require('../services/register-pool.js');
-const getPool = require('../services/get-pool.js');
+const getPools = require('../services/get-pool.js');
+const getPubPools = require('../services/get-pub-pools.js');
 const validate = require('../utils/validators.js');
+
+router.get('/public', async (req, res) => {
+  try {
+    const result = await getPubPools();
+
+    if (result.error) {
+      throw result.error;
+    }
+
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send({ error: true, message: error.message });
+  }
+});
 
 router.get('/', async (req, res) => {
   try {
